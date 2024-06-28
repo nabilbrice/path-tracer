b75788cafbb1d79e8508020d16b9958277598510
Nabil Brice <nabilbrice@yahoo.co.uk>
2024-06-28 12:49:20 +0100

    changed data structure for holding ppm data

--- src/driver_io.f90
+++ src/driver_io.f90
@@ -8,21 +8,25 @@ contains
   !> to be viewable
   subroutine save_to_ppm(filepath, data)
     character(len=*), intent(in) :: filepath
-    integer,          intent(in) :: data(:,:)
+    integer,          intent(in) :: data(:,:,:)
 
     integer :: file_handle
-    integer :: i, j
+    integer :: i, j, k, image_width, image_height
+    ! Obtain the image information from the data shape
+    image_width  = size(data,2)
+    image_height = size(data,3)
 
     open(newunit=file_handle, file=filepath)
-    
     ! Set up header
     write(file_handle, '(a)') "P3"
-    write(file_handle, '(i0, 1x, i0)') 3, 2
+    write(file_handle, '(i0, 1x, i0)') image_width, image_height
     write(file_handle, '(i0)') 255
 
     ! Write data in lines of 3 values
-    do j=1,size(data,2)
-       write(file_handle, '(3(i0, 1x))') (data(i,j), i=1,3)
+    do k=1,image_height
+       do j=1,image_width
+          write(file_handle, '(3(i0, 1x))') (data(i,j,k), i=1,3)
+       end do
     end do
 
     close(file_handle)
--- test/test_io.f90
+++ test/test_io.f90
@@ -11,7 +11,8 @@ contains
   subroutine collect_driver_io(testsuite)
     type(unittest_type), allocatable, intent(out) :: testsuite(:)
 
-    testsuite = [new_unittest("save to ppm", test_save_to_ppm)]
+    testsuite = [new_unittest("save to ppm", test_save_to_ppm), &
+                 new_unittest("larger ppm", test_larger_ppm)    ]
   end subroutine collect_driver_io
 
   !> Unit test for saving to ppm file with the standard data
@@ -20,7 +21,7 @@ contains
   subroutine test_save_to_ppm(error)
     type(error_type), allocatable, intent(out) :: error
 
-    integer, dimension(3,6) :: data
+    integer, dimension(3, 3, 2) :: data
 
     ! The standard data
     data = reshape( [ &
@@ -35,6 +36,24 @@ contains
     call save_to_ppm("./test.ppm", data)
     
   end subroutine test_save_to_ppm
+
+  subroutine test_larger_ppm(error)
+    type(error_type), allocatable, intent(out) :: error
+
+    integer, dimension(3,256,256) :: data
+    integer :: column, row
+
+    data = 0
+    do row=1,size(data,3)
+       do column=1,size(data,2)
+          data(1,column,row) = column
+          data(2,column,row) = row
+       end do
+    end do
+
+    call save_to_ppm("./background.ppm", data)
+
+  end subroutine test_larger_ppm
   
 end module test_io
 
